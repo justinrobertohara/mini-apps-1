cbfa5a03a26460a41199e2b8078a31f0eea106b5
Justin O'Hara <justinrobertohara@gmail.com>
2019-07-08 22:23:14 -0700

    add basic html and dom manipulation

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,6 @@
-
-
+function displayX(ID) {
+  // var square = document.getElementById(thisID);
+  // console.log(square);
+  // square.innerHTML = 'X';
+  document.getElementById(ID).innerHTML = 'X';
+}
