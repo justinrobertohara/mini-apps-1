5313241e1abdd1a9d2581a8692d7732af0802b8f
Justin O'Hara <justinrobertohara@gmail.com>
2019-07-08 22:38:07 -0700

    alterating xs and os

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,15 @@
+var bool = true;
+
 function displayX(ID) {
   // var square = document.getElementById(thisID);
   // console.log(square);
   // square.innerHTML = 'X';
-  document.getElementById(ID).innerHTML = 'X';
+
+  if (bool === true) {
+    document.getElementById(ID).innerHTML = 'X';
+    bool = false;
+  } else if (bool === false) {
+    document.getElementById(ID).innerHTML = 'O';
+    bool = true;
+  }
 }
