8520b25178e0389dfb7b461a43678ea733970cf0
Justin O'Hara <justinrobertohara@gmail.com>
2019-07-17 19:15:58 -0700

    rendered react components on static html

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,15 @@
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 
+ReactDOM.render(<App />, document.getElementById('root'));
